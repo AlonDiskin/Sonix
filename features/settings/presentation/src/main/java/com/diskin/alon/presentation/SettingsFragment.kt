792ab3d622a4d1d6a8065f2dd31f6469e9808b8c
Alon Diskin <alon.diskin@gmail.com>
2021-11-14 14:10:09 +0200

    Apply custom app theme that include text fonts and brand colors

--- features/settings/presentation/src/main/java/com/diskin/alon/presentation/SettingsFragment.kt
+++ features/settings/presentation/src/main/java/com/diskin/alon/presentation/SettingsFragment.kt
@@ -1,6 +1,19 @@
 package com.diskin.alon.presentation
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.fragment.app.Fragment
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+    }
 }
